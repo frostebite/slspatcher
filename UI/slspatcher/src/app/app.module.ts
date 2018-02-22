6f7f2845b209506af8b734c2a7978036baad2cbb
Frostebite <jas.f.ukcmti@gmail.com>
2018-02-22 19:57:43 +0000

    updates

--- UI/slspatcher/src/app/app.module.ts
+++ UI/slspatcher/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MatCardModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
