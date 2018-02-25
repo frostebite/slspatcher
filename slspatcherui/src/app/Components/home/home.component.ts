c021f1a0bca342c13801d12d8b4ee721ad9735ef
Frostebite <jas.f.ukcmti@gmail.com>
2018-02-25 12:17:50 +0000

    fix to game names

--- slspatcherui/src/app/Components/home/home.component.ts
+++ slspatcherui/src/app/Components/home/home.component.ts
@@ -11,19 +11,19 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
   apps:any[] = [
-    {name: "Turn of War"        , description: "A game about ...", status:"ready"},
-    {name: "Turn of War - WW2 " , description: "A game about ...", status:"update"},
-    {name: "Poser 3D"           , description: "A game about ...", status:"downloading"},
-    {name: "Squad MOBA"         , description: "A game about ...", status:"available"},
-    {name: "The Valley"         , description: "A game about ...", status:"ready"},
-    {name: "Area of Operations" , description: "A game about ...", status:"ready"},
-    {name: "Fantasy MMO"        , description: "A game about ...", status:"ready"},
-    {name: "Battle"             , description: "A game about ...", status:"ready"},
-    {name: "Fury Road MMO"      , description: "A game about ...", status:"ready"},
-    {name: "Western MMO"        , description: "A game about ...", status:"ready"},
-    {name: "SWAT/HVT"           , description: "A game about ...", status:"ready"},
-    {name: "Paranormal"         , description: "A game about ...", status:"ready"},
-    {name: "Space?"             , description: "A game about ...", status:"ready"},
-    {name: "Top Down/OTS Base"  , description: "A game about ...", status:"ready"},
+    {name: "Turn of War"            , description: "A game about ...", status:"ready"},
+    {name: "Turn of War - WW2 "     , description: "A game about ...", status:"update"},
+    {name: "Poser 3D"               , description: "A game about ...", status:"downloading"},
+    {name: "Squad MOBA"             , description: "A game about ...", status:"available"},
+    {name: "The Valley"             , description: "A game about ...", status:"ready"},
+    {name: "Area of Operations"     , description: "A game about ...", status:"ready"},
+    {name: "Fantasy MMO"            , description: "A game about ...", status:"ready"},
+    {name: "Battle"                 , description: "A game about ...", status:"ready"},
+    {name: "Fury Road MMO"          , description: "A game about ...", status:"ready"},
+    {name: "Western MMO"            , description: "A game about ...", status:"ready"},
+    {name: "SWAT/HVT"               , description: "A game about ...", status:"ready"},
+    {name: "Paranormal"             , description: "A game about ...", status:"ready"},
+    {name: "Space?"                 , description: "A game about ...", status:"ready"},
+    {name: "Top Down/OTS Survival"  , description: "A game about ...", status:"ready"},
   ];
 }
